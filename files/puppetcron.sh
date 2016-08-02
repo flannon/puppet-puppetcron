2ef86908cf9fe383b823b6fa4d1eb502ac97acff
Flannon Jackson <flannon@nyu.edu>
2016-08-02 13:01:36 -0400

    moved 10k update to it's own script.  puppetcron.sh now only runs puppet.

--- files/puppetcron.sh
+++ files/puppetcron.sh
@@ -16,25 +16,6 @@ MANIFEST="$BASEDIR/manifests/site.pp"
 MODULEDIR="$BASEDIR/modules/thirdparty"
 PUPPETFILE="$BASEDIR/Puppetfile"
 
-# Make sure the latest version of thepupet repo is installed
-puppet apply /etc/puppet/manifests/puppetcron.pp
-
-cd $BASEDIR
-
-if [[ ! -d $MODULEDIR ]]
-then
-    mkdir -p $MODULEDIR
-fi
-
-if [[ -f $PUPPETFILE ]] 
-then
-    #/usr/local/rvm/gems/ruby-1.9.3-p551/bin/r10k puppetfile install --moduledir $MODULEDIR
-    r10k puppetfile install 
-else 
-    echo "$PUPPETFILE file not found    "
-    exit 257
-fi
-
 if [[ -f $MANIFEST ]]
 then
     /usr/bin/puppet apply $MANIFEST
@@ -43,7 +24,3 @@ else
     exit 258
 fi
 
-echo ''
-echo ''
-echo ''
-echo ''
